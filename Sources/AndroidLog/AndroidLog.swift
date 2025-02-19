7ec4b58836ebfbcc8ba2fdf47ceb4cdb889176f4
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2025-02-19 12:47:19 +0300

    no message

--- Sources/AndroidLog/AndroidLog.swift
+++ Sources/AndroidLog/AndroidLog.swift
@@ -1,5 +1,5 @@
 import CAndroidLog
 
 public func android_log(priority: AndroidLogPriority = .info, tag: String = "swift", _ string: String) {
-    android_log(Int32(priority.logPriority.rawValue), tag, string)
+    android_log(CInt(priority.logPriority.rawValue), tag, string)
 }
