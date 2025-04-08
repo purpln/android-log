9168f7bfe0cf24abe94fafff8f2a30e437ef7877
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2025-04-09 00:08:08 +0300

    no message

--- Sources/AndroidLog/AndroidLog.swift
+++ Sources/AndroidLog/AndroidLog.swift
@@ -1,5 +1,5 @@
 import LibC
 
 public func android_log(priority: AndroidLogPriority = .info, tag: String = "threaded_app", _ string: String) {
-    android_log(CInt(priority.logPriority.rawValue), tag, string)
+    android_log(priority.logPriority, tag, string)
 }
