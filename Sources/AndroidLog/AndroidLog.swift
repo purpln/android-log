96955ff8db34619c8ba48eec2302b04703b5ab8c
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2025-05-06 15:56:20 +0300

    no message

--- Sources/AndroidLog/AndroidLog.swift
+++ Sources/AndroidLog/AndroidLog.swift
@@ -1,7 +1,7 @@
 import Android
 
 @_silgen_name("__android_log_print")
-private func android_log(
+private func android_log_print(
     priority: android_LogPriority,
     tag: UnsafePointer<CChar>,
     /*format: UnsafePointer<CChar>,*/
@@ -10,8 +10,8 @@ private func android_log(
 
 public func android_log(
     priority: AndroidLogPriority = .info,
-    tag: String = "threaded_app",
-    _ message: String
+    tag: String,
+    message: String
 ) {
-    android_log(priority: priority.logPriority, tag: tag, message: message)
+    android_log_print(priority: priority.logPriority, tag: tag, message: message)
 }
